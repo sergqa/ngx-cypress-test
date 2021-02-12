70670c989aec65466428e81d596a6053e04a3e21
Serhii Kluban <serhii_kluban@epam.com>
2021-02-12 11:51:09 +0200

    secondTest

--- cypress/integration/firstStep.spec.js
+++ cypress/integration/firstStep.spec.js
@@ -37,4 +37,38 @@ describe('Our first suite', () => {
 
   })
 
+  it('second test', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    cy.contains('Sign in')
+    cy.contains('[status="warning"]','Sign in')
+
+    cy.get('#inputEmail3')
+      .parents('Form')
+      .find('button')
+      .should('contain', 'Sign in')
+      .parents('Form')
+      .find('nb-checkbox')
+      .click()
+
+    cy.contains('nb-card', 'Horizontal form')
+      .find('[type="Email"]')
+  })
+
+  it.only('then and wrap methods', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+    cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+    cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email address')
+    cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+
+
+  })
+
 })
